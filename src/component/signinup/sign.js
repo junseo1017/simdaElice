0812fd039a3e498ae377e599970e94de7456fa0e
junseolee <dlwnstjzld@gmail.com>
2022-05-09 20:46:43 +0900

    sign

--- src/component/signinup/sign.js
+++ src/component/signinup/sign.js
@@ -1,22 +1,58 @@
-import React, {useState, memo, useRef} from 'react';
+import React, {useState, memo, useRef, useCallback, useReducer} from 'react';
 import './sign.css';
 import {Typography, Input, Button} from 'antd';
 import axios from 'axios';
-import {useNavigate, Link, Navigate} from 'react-router-dom';
+import './diarycontent.js';
+import {useNavigate, Link} from 'react-router-dom';
 
-const reg = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+const initialState = {
+  emailAlert: {message: '', status: ''},
+  passwordAlert: {message: '', status: ''},
+  pwCheckAlert: {message: '', status: ''},
+};
 
-const SignUp = memo(() => {
-  const [emailAlert, setEmailAlert] = useState([{message: '', status: ''}]);
-  const [passwordAlert, setPasswordAlert] = useState([{message: '', status: ''}]);
-  const [pwCheckAlert, setPwCheckAlert] = useState([{message: '', status: ''}]);
+const action = {
+  EMAILCHECK: 'EMAILCHECK',
+  PASSWORDCHECK: 'PASSWORDCHECK',
+  PWCHECK: 'PWCHECK',
+};
+
+const reducer = (alert, action) => {
+  const {EMAILCHECK, PASSWORDCHECK, PWCHECK} = action;
+  switch (action.type) {
+    case EMAILCHECK:
+      return {
+        ...alert,
+        emailAlert: action.emailAlert,
+      };
+    case PASSWORDCHECK:
+      return {
+        ...alert,
+        passwordAlert: action.passwordAlert,
+      };
+    case PWCHECK:
+      return {
+        ...alert,
+        pwCheck: action.pwCheckAlert,
+      };
+    default:
+      return {
+        ...alert,
+      };
+  }
+};
+
+const SignUp = () => {
+  const [alert, dispatch] = useReducer(reducer, initialState);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [pwCheck, setPwCheck] = useState('');
-  // 회원가입 버튼 중복 클릭 방지
+  const reg = useRef(/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i);
+
   const clickable = useRef(false);
   const navigate = useNavigate();
+
   async function getPost() {
     try {
       const response = await axios.post('http://14.35.100.207:3000/users/regist', {
@@ -24,9 +60,9 @@ const SignUp = memo(() => {
         user_name: name,
         user_password: password,
       });
-      console.log(response);
-    } catch (error) {
-      console.err(error);
+      await navigate(`./diary.js`);
+    } catch (err) {
+      console.error(err);
     }
   }
 
@@ -36,45 +72,62 @@ const SignUp = memo(() => {
       setName(e.target.value);
     } else if (e.target.id === 'emailInput') {
       setEmail(e.target.value);
-      reg.test(email)
-        ? setEmailAlert([{message: '유효한 이메일입니다.', status: 'success'}])
-        : setEmailAlert([{message: '이메일 형식에 맞게 작성해주세요.', status: 'warning'}]);
+      reg.current.test(email)
+        ? dispatch({
+            type: 'EMAILCHECK',
+            emailAlert: {message: '유효한 이메일입니다.', status: 'success'},
+          })
+        : dispatch({
+            type: 'EMAILCHECK',
+            emailAlert: {message: '이메일 형식에 맞게 작성해주세요.', status: 'warning'},
+          });
     } else if (e.target.id === 'pwInput') {
       setPassword(e.target.value);
       password.length >= 8
-        ? setPasswordAlert([{message: '유효한 비밀번호입니다.', status: 'success'}])
-        : setPasswordAlert([{message: '비밀번호는 8자 이상 입력해주세요', status: 'warning'}]);
+        ? dispatch({
+            type: 'PASSWORDCHECK',
+            passwordAlert: {message: '유효한 비밀번호입니다.', status: 'success'},
+          })
+        : dispatch({
+            type: 'PASSWORDCHECK',
+            passwordAlert: {message: '비밀번호는 8자 이상 입력해주세요', status: 'warning'},
+          });
     } else if (e.target.id === 'pwCkInput') {
       setPwCheck(e.target.value);
     }
+    console.log(name, email, password, pwCheck);
   };
 
-  const onClick = (e) => {
+  const onClick = useCallback((e) => {
     e.preventDefault();
     if (!clickable.current) return;
     if (!email && !name && !password && !pwCheck) return;
-    // input 유효성 검사
     if (!reg.test(email)) return;
-    // email 유효성 검사
     if (pwCheck !== password || password.length < 8) {
-      // 비밀번호 유효성 검사
       passwordFunc();
       return;
     }
-    getPost();
+    // getPost();
+    console.log(name);
     console.log(email);
     console.log(password);
-    navigate(`./diary.js`);
+    console.log('done');
     clickable.current = false;
-  };
+  }, []);
 
-  const passwordFunc = () => {
-    setPwCheckAlert([{message: '비밀번호가 일치하지 않습니다.', status: 'warning'}]);
+  const passwordFunc = useCallback(() => {
+    dispatch({
+      type: 'PWCHECK',
+      pwCheckAlert: {message: '비밀번호가 일치하지 않습니다.', status: 'warning'},
+    });
     setTimeout(() => {
-      setPwCheckAlert([{message: '', status: ''}]);
+      dispatch({
+        type: 'PWCHECK',
+        pwCheckAlert: {message: '', status: ''},
+      });
     }, 1000);
     return;
-  };
+  }, []);
 
   return (
     <>
@@ -89,17 +142,17 @@ const SignUp = memo(() => {
           <div className='inputContainer'>
             <Typography.Title level={5}>Email</Typography.Title>
             <Input size='large' type='text' id='emailInput' onChange={onChange}></Input>
-            <Typography.Text type={emailAlert[0].status}>{emailAlert[0].message}</Typography.Text>
+            <Typography.Text type={alert.emailAlert.status}>{alert.emailAlert.message}</Typography.Text>
           </div>
           <div className='inputContainer'>
             <Typography.Title level={5}>Password</Typography.Title>
             <Input size='large' type='password' id='pwInput' onChange={onChange}></Input>
-            <Typography.Text type={passwordAlert[0].status}>{passwordAlert[0].message}</Typography.Text>
+            <Typography.Text type={alert.passwordAlert.status}>{alert.passwordAlert.message}</Typography.Text>
           </div>
           <div className='inputContainer'>
             <Typography.Title level={5}>Check Password</Typography.Title>
             <Input size='large' type='password' id='pwCkInput' onChange={onChange}></Input>
-            <Typography.Text type={pwCheckAlert[0].status}>{pwCheckAlert[0].message}</Typography.Text>
+            <Typography.Text type={alert.pwCheckAlert.status}>{alert.pwCheckAlert.message}</Typography.Text>
           </div>
           <Button type='primary' size='large' onClick={onClick}>
             Complete Sign up!
@@ -108,6 +161,6 @@ const SignUp = memo(() => {
       </div>
     </>
   );
-});
+};
 
 export default SignUp;
