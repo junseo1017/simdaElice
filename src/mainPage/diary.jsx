9d52ad8eaedf78dc4bc71a71fbd65b9e98b61ed4
junseolee <dlwnstjzld@gmail.com>
2022-05-14 15:20:53 +0900

    fix diarylist

--- src/mainPage/diary.jsx
+++ src/mainPage/diary.jsx
@@ -1,13 +1,14 @@
-import React, { useState, memo } from "react";
-import { Tabs, Calendar, Badge } from "antd";
-import "antd/dist/antd.min.css";
+import React, {useState, memo, useEffect, useRef} from 'react';
+import {Tabs, Calendar, Badge} from 'antd';
+import 'antd/dist/antd.min.css';
+import DiaryTopData from './component/DiaryTopData.jsx';
+import DairyContent from './component/diarycontent.js';
+import ImageUpload from './component/ImageUpload.js';
+import CalendarModal from './component/CalendarModal.jsx';
+import DiaryList from './component/diaryList.js';
+import axios from 'axios';
 
-import DiaryTopData from "./component/DiaryTopData.jsx";
-import DairyContent from "./component/diarycontent.js";
-import ImageUpload from "./component/ImageUpload.js";
-import CalendarModal from "./component/CalendarModal.jsx";
-
-const { TabPane } = Tabs;
+const {TabPane} = Tabs;
 
 function getListData(value) {
   let listData;
@@ -23,7 +24,7 @@ function dateCellRender(value) {
   const listData = getListData(value);
   // console.log(listData);
   return (
-    <ul className="">
+    <ul className=''>
       {listData.map((item) => (
         <li key={item.content}>
           <Badge status={item.type} text={item.content} />
@@ -58,27 +59,62 @@ function monthCellRender(value) {
 const Diary = memo(() => {
   const [topData, setTopData] = useState({});
   const [clickTapOne, setClickTapOne] = useState(false);
+  const [diaryContent, setDiaryContent] = useState('');
+  const listData = useRef();
+
   //const wholeData = useMemo(() => requestData(), []);
   //return에           <ImageUpload /> 수정예정
   const onClickTap = (e) => {
-    if (e === "1") {
+    if (e === '1') {
       setClickTapOne(true);
     }
   };
+
+  useEffect(() => {
+    getDiaryList();
+  }, [listData]);
+
+  async function getDiaryList() {
+    try {
+      const res = await axios.get('http://14.35.100.207:3000/diarys/info');
+      listData.current = res.data.diaryInfoList;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  async function postMainPageSubmit() {
+    // try {
+    //   const res = await axios.post('http://14.35.100.207:3000/diarys/regist', {
+    //     diary_seq:0,
+    //     diary_reg_date: `${state.date[0]-${state.date[1]}-${state.date[2]}`,
+    //     diary_weather_type: state.weather,
+    //     diary_feel_type: state.emotion,
+    //     diary_wakeup_time: '00:00:00',
+    //     diary_sleep_time: '00:00:00',
+    //     diary_content: diaryContent,
+    //   });
+    // } catch (err) {
+    //   console.error(err);
+    // }
+  }
+  const onClick = () => {
+    postMainPageSubmit();
+    console.log(listData.current);
+    // 스테이트 초기화 시켜야함
+  };
   return (
     <>
-      <Tabs tabPosition={"right"}>
-        <TabPane tab="Calender" key="1">
+      <Tabs tabPosition={'right'}>
+        <TabPane tab='Calender' key='1'>
           <DiaryTopData setTopData={setTopData} />
-          <CalendarModal
-            setclickValue={setClickTapOne}
-            clickValue={clickTapOne}
-          />
+          <CalendarModal setclickValue={setClickTapOne} clickValue={clickTapOne} />
 
           <DairyContent />
         </TabPane>
-        <TabPane tab="Diary List" key="2"></TabPane>
-        <TabPane tab="Set Up" key="3"></TabPane>
+        <TabPane tab='Diary List' key='2'>
+          <DiaryList listData={listData} />
+        </TabPane>
+        <TabPane tab='Set Up' key='3'></TabPane>
       </Tabs>
     </>
   );
