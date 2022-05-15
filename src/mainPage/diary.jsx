cc5f3ac00c5fa9bd6cf5418acd6e60980ff484b9
Kanghunk <ltbehappy16@gmail.com>
2022-05-15 23:28:13 +0900

    merge calendar branch

--- src/mainPage/diary.jsx
+++ src/mainPage/diary.jsx
@@ -8,18 +8,23 @@ import ImageUpload from './component/ImageUpload.js';
 import CalendarModal from './component/CalendarModal.jsx';
 import DiaryList from './component/diaryList.js';
 import axios from 'axios';
-
-const { TabPane } = Tabs;
 import "./component/layout/diary.css";
 
 const { TabPane } = Tabs;
 
+
 const Diary = memo(() => {
-    const [topData, setTopData] = useState({});
-    const [clickModal, setclickModal] = useState(false);
-    const [currentTap, setCurrentTap] = useState('1');
-    const [diaryContent, setDiaryContent] = useState('');
-    const listData = useRef();
+
+  const [topData, setTopData] = useState({});
+  const [clickModal, setclickModal] = useState(false);
+  const [currentTap, setCurrentTap] = useState('1');
+  // ImageUpload에 props로 보낼 state와 setState
+  const [images, setImages] = useState([]);
+  const [clickTapOne, setClickTapOne] = useState(false);
+  const [diaryContent, setDiaryContent] = useState('');
+
+
+  const listData = useRef();
 
     //const wholeData = useMemo(() => requestData(), []);
     //return에           <ImageUpload /> 수정예정
@@ -60,7 +65,31 @@ const Diary = memo(() => {
         //   console.error(err);
         // }
     }
-    const onClick = () => {
+
+  useEffect(() => {
+    getDiaryList();
+  }, [listData]);
+
+  
+  async function postMainPageSubmit() {
+    // try {
+    //   const res = await axios.post('http://14.35.100.207:3000/diarys/regist', {
+    //     diary_seq:0,
+    //     diary_reg_date: `${state.date[0]-${state.date[1]}-${state.date[2]}`,
+    //     diary_weather_type: state.weather,
+    //     diary_feel_type: state.emotion,
+    //     diary_wakeup_time: '00:00:00',
+    //     diary_sleep_time: '00:00:00',
+    //     diary_content: diaryContent,
+    //     diary_img:image[0].data_url,
+    //   });
+    // } catch (err) {
+    //   console.error(err);
+    // }
+    console.log('send file');
+  }
+
+  const onClick = () => {
     if (window.confirm("저장하시겠습니까?")) {
       postMainPageSubmit();
       console.log(listData.current);
@@ -74,7 +103,7 @@ const Diary = memo(() => {
                 <TabPane tab='Diary' key='1'>
                     <DiaryTopData setTopData={setTopData} />
                     <ImageUpload images={images} onUploadImage={setImages} />
-                    <DairyContent onClick={onClick} />
+                    <DiaryContent setDiaryContent={setDiaryContent} onClick={onClick} />
                 </TabPane>
                 <TabPane tab='Calender' key='2'>
                     <CalendarModal setclickModal={setclickModal} clickModal={clickModal} setCurrentTap={setCurrentTap} getDiaryList={getDiaryList} />
