37167b47e4a6e623303fceb5e07dde2676020d21
Kanghunk <ltbehappy16@gmail.com>
2022-05-18 05:46:42 +0900

    fix calendar_merge

--- src/mainPage/component/diaryListComponent/modalComponent.js
+++ src/mainPage/component/diaryListComponent/modalComponent.js
@@ -2,21 +2,12 @@ import React, {memo, useCallback} from 'react';
 import {Modal} from 'antd';
 
 const ModalComponent = ({visible, handleOk, handleCancel, confirmLoading, sortList, modalData}) => {
-  const {diary_content, update_time, diary_feel_type, diary_weather_type} = modalData.current;
+  const {diary_content, update_time, diary_seq, diary_feel_type, diary_weather_type} = modalData.current;
+
   const changeWeatherData = useCallback((data) => {
     switch (data) {
       case 1:
         return '맑음';
-      case 2:
-        return '흐림';
-      case 3:
-        return '비 내림';
-      case 4:
-        return '번개침';
-      case 5:
-        return '바람 붐';
-      case 6:
-        return '눈 내림';
       default:
         return;
     }
@@ -25,17 +16,7 @@ const ModalComponent = ({visible, handleOk, handleCancel, confirmLoading, sortLi
   const changeFeelData = useCallback((data) => {
     switch (data) {
       case 1:
-        return '행복';
-      case 2:
-        return '별로';
-      case 3:
-        return '좋음';
-      case 4:
-        return '신남';
-      case 5:
-        return '분노';
-      case 6:
-        return '우울';
+        return '기분 좋음';
       default:
         return;
     }
@@ -43,20 +24,15 @@ const ModalComponent = ({visible, handleOk, handleCancel, confirmLoading, sortLi
   return (
     <>
       <Modal mask={true} maskStyle={{backgroundColor: 'transparent'}} onOk={handleOk} visible={visible} confirmLoading={confirmLoading} onCancel={handleCancel}>
-        <h1>{update_time.slice(0, 10)}</h1>
-        <div>
-          <h2>
-            <b>오늘의 날씨 : {changeWeatherData(diary_weather_type)}</b>
-          </h2>
-        </div>
-        <div>
-          <h2>
-            <b>오늘의 기분 : {changeFeelData(diary_feel_type)}</b>
-          </h2>
-        </div>
-        <div className='paper'>
-          <div className='paper-content'>
-            <textarea value={diary_content}></textarea>
+        <div className='modalContentContainer'>
+          <h1>{update_time.slice(0, 10)}</h1>
+          <span>{changeWeatherData(diary_weather_type)}</span>
+          <span>{changeFeelData(diary_feel_type)}</span>
+          <div>{diary_seq}</div>
+          <div className='paper'>
+            <div className='paper-content'>
+              <textarea value={diary_content}></textarea>
+            </div>
           </div>
         </div>
       </Modal>
--- src/mainPage/diary.jsx
+++ src/mainPage/diary.jsx
@@ -1,5 +1,6 @@
-import React, {useState, memo, useEffect, useCallback, useRef} from 'react';
-import {Tabs} from 'antd';
+
+import React, { useState, memo, useEffect, useCallback, useRef } from 'react';
+import { Tabs } from 'antd';
 import 'antd/dist/antd.min.css';
 import DiaryTopData from './component/DiaryTopData.jsx';
 import DairyContent from './component/diarycontent.js';
@@ -7,11 +8,13 @@ import ImageUpload from './component/ImageUpload.js';
 import CalendarModal from './component/CalendarModal.jsx';
 import DiaryList from './component/diaryList.js';
 import axios from 'axios';
-import './component/layout/diary.css';
+import "./component/layout/diary.css";
 
-const {TabPane} = Tabs;
+const { TabPane } = Tabs;
+
 
 const Diary = memo(() => {
+
   const [topData, setTopData] = useState({});
   const [clickModal, setclickModal] = useState(false);
   const [currentTap, setCurrentTap] = useState({cur: '1', prev: ''});
@@ -20,9 +23,9 @@ const Diary = memo(() => {
 //   const [clickTapOne, setClickTapOne] = useState(false);
   const [diaryContent, setDiaryContent] = useState('');
 
+
   const listData = useRef();
 
-  
     //const wholeData = useMemo(() => requestData(), []);
     //return에           <ImageUpload /> 수정예정
     const onClickTap = (e) => {
@@ -34,47 +37,62 @@ const Diary = memo(() => {
         }
     };
 
-    
+    useEffect(() => {
+        getDiaryList();
+    }, [listData]);
+
+  
+    const getDiaryList = useCallback(async () => {
+        try {
+            const res = await axios.get('http://14.35.100.207:3000/diarys/info');
+            const getData = res.data.diaryInfoList;
+            listData.current = getData;
+            return getData;
+        } catch (err) {
+            console.log(err);
+        }
+    })
+    async function postMainPageSubmit() {
+        // try {
+        //   const res = await axios.post('http://14.35.100.207:3000/diarys/regist', {
+        //     diary_seq:0,
+        //     diary_reg_date: `${state.date[0]-${state.date[1]}-${state.date[2]}`,
+        //     diary_weather_type: state.weather,
+        //     diary_feel_type: state.emotion,
+        //     diary_wakeup_time: '00:00:00',
+        //     diary_sleep_time: '00:00:00',
+        //     diary_content: diaryContent,
+        //   });
+        // } catch (err) {
+        //   console.error(err);
+        // }
     }
-    setCurrentTap(e);
-  };
 
   useEffect(() => {
     getDiaryList();
   }, [listData]);
 
-  const getDiaryList = useCallback(async () => {
-    try {
-      const res = await axios.get('http://14.35.100.207:3000/diarys/info');
-      const getData = res.data.diaryInfoList;
-      listData.current = getData;
-      return getData;
-    } catch (err) {
-      console.log(err);
-    }
-  });
-
+  
   async function postMainPageSubmit() {
-    try {
-      const res = await axios.post('http://14.35.100.207:3000/diarys/info/regist', {
-        user_seq: 17,
-        diary_seq: 5,
-        diary_reg_date: `${topData.date[0]}-${topData.date[1]}-${topData.date[2]}`,
-        diary_weather_type: topData.weather,
-        diary_feel_type: topData.emotion,
-        diary_content: diaryContent,
-      });
-    } catch (err) {
-      console.error(err);
-    }
+    // try {
+    //   const res = await axios.post('http://14.35.100.207:3000/diarys/regist', {
+    //     diary_seq:0,
+    //     diary_reg_date: `${state.date[0]-${state.date[1]}-${state.date[2]}`,
+    //     diary_weather_type: state.weather,
+    //     diary_feel_type: state.emotion,
+    //     diary_wakeup_time: '00:00:00',
+    //     diary_sleep_time: '00:00:00',
+    //     diary_content: diaryContent,
+    //     diary_img:image[0].data_url,
+    //   });
+    // } catch (err) {
+    //   console.error(err);
+    // }
+    console.log('send file');
   }
 
-  useEffect(() => {
-    getDiaryList();
-  }, [listData]);
-
   const onClick = () => {
-    if (window.confirm('저장하시겠습니까?')) {
+    if (window.confirm("저장하시겠습니까?")) {
       postMainPageSubmit();
       console.log(listData.current);
       window.location.reload();
@@ -99,7 +117,6 @@ const Diary = memo(() => {
             </Tabs>
         </>
     );
-
 });
 
 export default Diary;
