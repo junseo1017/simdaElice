b719fa685838876ae4fd665fcd9bbf8381730228
Kanghunk <ltbehappy16@gmail.com>
2022-05-18 06:41:56 +0900

    수정 마무리

--- src/mainPage/diary.jsx
+++ src/mainPage/diary.jsx
@@ -17,79 +17,56 @@ const Diary = memo(() => {
 
   const [topData, setTopData] = useState({});
   const [clickModal, setclickModal] = useState(false);
-  const [currentTap, setCurrentTap] = useState({cur: '1', prev: ''});
+  const [currentTap, setCurrentTap] = useState({ cur: '1', prev: '' });
   // ImageUpload에 props로 보낼 state와 setState
   const [images, setImages] = useState([]);
-//   const [clickTapOne, setClickTapOne] = useState(false);
+  const [clickTapOne, setClickTapOne] = useState(false);
   const [diaryContent, setDiaryContent] = useState('');
 
 
   const listData = useRef();
 
-    //const wholeData = useMemo(() => requestData(), []);
-    //return에           <ImageUpload /> 수정예정
-    const onClickTap = (e) => {
-        if (e === '2') {
-            setclickModal(true);
-            setCurrentTap((prev) => ({...prev, cur: e, prev: prev.cur}));
-        } else {
-            setCurrentTap((prev) => ({...prev, cur: e}));
-        }
-    };
-
-    useEffect(() => {
-        getDiaryList();
-    }, [listData]);
-
-  
-    const getDiaryList = useCallback(async () => {
-        try {
-            const res = await axios.get('http://14.35.100.207:3000/diarys/info');
-            const getData = res.data.diaryInfoList;
-            listData.current = getData;
-            return getData;
-        } catch (err) {
-            console.log(err);
-        }
-    })
-    async function postMainPageSubmit() {
-        // try {
-        //   const res = await axios.post('http://14.35.100.207:3000/diarys/regist', {
-        //     diary_seq:0,
-        //     diary_reg_date: `${state.date[0]-${state.date[1]}-${state.date[2]}`,
-        //     diary_weather_type: state.weather,
-        //     diary_feel_type: state.emotion,
-        //     diary_wakeup_time: '00:00:00',
-        //     diary_sleep_time: '00:00:00',
-        //     diary_content: diaryContent,
-        //   });
-        // } catch (err) {
-        //   console.error(err);
-        // }
+  //const wholeData = useMemo(() => requestData(), []);
+  //return에           <ImageUpload /> 수정예정
+  const onClickTap = (e) => {
+    if (e === '2') {
+      setclickModal(true);
+      setCurrentTap((prev) => ({ ...prev, cur: e, prev: prev.cur }));
+    } else {
+      setCurrentTap((prev) => ({ ...prev, cur: e }));
     }
+  };
+
+  const getDiaryList = useCallback(async () => {
+    try {
+      const res = await axios.get('http://14.35.100.207:3000/diarys/info');
+      const getData = res.data.diaryInfoList;
+      listData.current = getData;
+      return getData;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+
+  async function postMainPageSubmit() {
+    try {
+      const res = await axios.post('http://14.35.100.207:3000/diarys/info/regist', {
+        user_seq: 17,
+        diary_seq: 5,
+        diary_reg_date: `${topData.date[0]}-${topData.date[1]}-${topData.date[2]}`,
+        diary_weather_type: topData.weather,
+        diary_feel_type: topData.emotion,
+        diary_content: diaryContent,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   useEffect(() => {
     getDiaryList();
   }, [listData]);
-
-  
-  async function postMainPageSubmit() {
-    // try {
-    //   const res = await axios.post('http://14.35.100.207:3000/diarys/regist', {
-    //     diary_seq:0,
-    //     diary_reg_date: `${state.date[0]-${state.date[1]}-${state.date[2]}`,
-    //     diary_weather_type: state.weather,
-    //     diary_feel_type: state.emotion,
-    //     diary_wakeup_time: '00:00:00',
-    //     diary_sleep_time: '00:00:00',
-    //     diary_content: diaryContent,
-    //     diary_img:image[0].data_url,
-    //   });
-    // } catch (err) {
-    //   console.error(err);
-    // }
-    console.log('send file');
-  }
 
   const onClick = () => {
     if (window.confirm("저장하시겠습니까?")) {
@@ -99,24 +76,24 @@ const Diary = memo(() => {
     }
   };
 
-    
-    return (
-        <>
-            <Tabs tabPosition={'right'} onTabClick={onClickTap} activeKey={currentTap.cur}>
-                <TabPane tab='Diary' key='1'>
-                    <DiaryTopData setTopData={setTopData} />
-                    <ImageUpload images={images} onUploadImage={setImages} />
-                    <DairyContent setDiaryContent={setDiaryContent} onClick={onClick} />
-                </TabPane>
-                <TabPane tab='Calender' key='2'>
-                    <CalendarModal setclickModal={setclickModal} clickModal={clickModal} setCurrentTap={setCurrentTap} getDiaryList={getDiaryList} />
-                </TabPane>
-                <TabPane tab='Diary List' key='3'>
-                    <DiaryList listData={listData} />
-                </TabPane>
-            </Tabs>
-        </>
-    );
+
+  return (
+    <>
+      <Tabs tabPosition={'right'} onTabClick={onClickTap} activeKey={currentTap.cur}>
+        <TabPane tab='Diary' key='1'>
+          <DiaryTopData setTopData={setTopData} />
+          <ImageUpload images={images} onUploadImage={setImages} />
+          <DairyContent setDiaryContent={setDiaryContent} onClick={onClick} />
+        </TabPane>
+        <TabPane tab='Calender' key='2'>
+          <CalendarModal setclickModal={setclickModal} clickModal={clickModal} setCurrentTap={setCurrentTap} getDiaryList={getDiaryList} />
+        </TabPane>
+        <TabPane tab='Diary List' key='3'>
+          <DiaryList listData={listData} />
+        </TabPane>
+      </Tabs>
+    </>
+  );
 });
 
 export default Diary;
